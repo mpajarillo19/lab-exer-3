ff63f16315294abfeef812f6bade365ce8238660
Miguel Bryan Pajarillo <46342257+mpajarillo19@users.noreply.github.com>
2019-01-03 18:40:10 +0800

    Update Number 2.cpp

--- Number 2.cpp
+++ Number 2.cpp
@@ -7,7 +7,8 @@ int main(){
 	cout << "Enter a character: ";
 	cin >> character;
 	
-	cout <<"The value of character is " <<int(character);
+	cout <<"The value of this character is ";
+	cout  <<int(character);
 	
 	system ("pause > 0");
 	return 0;
